10eac523ca34308a40922178738e6fea7dac6d40
Stefan Niederhauser <ghuder5@gmx.ch>
2018-01-10 13:44:15 +0100

    add example test

--- src/test/guru/nidi/brainfuck/ExamplesTest.java
+++ src/test/guru/nidi/brainfuck/ExamplesTest.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ExamplesTest {
@@ -19,7 +20,8 @@ class ExamplesTest {
         String name = "beep.bf";
         final File target = new File("target");
         new File(target, name).delete();
-        BrainfuckExamples.writeToFile(name, target);
+        assertTrue(BrainfuckExamples.writeToFile(name, target));
+        assertFalse(BrainfuckExamples.writeToFile(name, target));
         assertTrue(new File(target, name).length() == 8);
     }
 }
